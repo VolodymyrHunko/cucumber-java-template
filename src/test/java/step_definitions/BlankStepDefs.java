6860b4c32bc1a1b78282305c0bb239e0e094384d
Volter <gunko63@gmail.com>
2018-01-23 18:30:58 -0500

    First commit

--- src/test/java/step_definitions/BlankStepDefs.java
+++ src/test/java/step_definitions/BlankStepDefs.java
@@ -17,14 +17,12 @@ public class BlankStepDefs{
     }
     
     @When("^I open seleniumframework website$")
-    public void i_open_seleniumframework_website() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    public void i_open_seleniumframework_website()  {
         driver.get("http://www.seleniumframework.com");
     }
 
     @Then("^I validate title and URL$")
-    public void i_print_title_and_URL() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    public void i_print_title_and_URL()  {
       assertEquals("Selenium Framework | Selenium, Cucumber, Ruby, Java et al.",driver.getTitle());
       assertEquals("http://www.seleniumframework.com/",driver.getCurrentUrl());
     }
--- src/test/java/step_definitions/RunCukesTest.java
+++ src/test/java/step_definitions/RunCukesTest.java
@@ -1,17 +1,16 @@
 package step_definitions;
 
+import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
+
 import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
+@CucumberOptions (
 		features = "classpath:features",
 		plugin = {"pretty", "html:target/cucumber-html-report"},
 		tags = {}
 		)
-public class RunCukesTest{
-	
-}
+public class RunCukesTest{ }
